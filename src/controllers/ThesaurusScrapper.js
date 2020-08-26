adafd0449ac876545051ae1b7bb55466768823be
Jorgen-Jr <jorgenjr@outlook.com>
2020-08-26 13:45:04 -0300

    Innit thesaurus dict.

--- src/controllers/ThesaurusScrapper.js
+++ src/controllers/ThesaurusScrapper.js
@@ -1,3 +1,12 @@
+import rp from 'request-promise';
+import $ from 'cheerio';
+
+const url = "https://www.dictionary.com/browse/";
+
 module.exports = {
-    
+    async getWordDefinition(word){
+        return rp(url + word).then(async (html) => {
+        
+        });
+    }
 }
