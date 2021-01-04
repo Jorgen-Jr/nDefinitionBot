cd0140f29ad9e0654b8d46dfd30bb2453c63c982
Jorgen-Jr <jorgenjr@outlook.com>
2021-01-04 14:26:31 -0300

    Updated priberam controlelr.

--- src/controllers/PriberamController.ts
+++ src/controllers/PriberamController.ts
@@ -10,39 +10,41 @@ export default async (word: string) => {
       return res.data;
     });
 
-  if (definitionPriberam.definition.length > 0) {
-    const definitionsPriberam = definitionPriberam.definition.map(
-      (def: DefinitionObject) => {
-        if (def.index === 0) {
-          return "<b><i>" + def.definition + "</i></b>";
-        } else {
-          return "<b><i>" + def.index + "</i></b> " + def.definition;
+  if (definitionPriberam.definition) {
+    if (definitionPriberam.definition.length > 0) {
+      const definitionsPriberam = definitionPriberam.definition.map(
+        (def: DefinitionObject) => {
+          if (def.index === 0) {
+            return "<b><i>" + def.definition + "</i></b>";
+          } else {
+            return "<b><i>" + def.index + "</i></b> " + def.definition;
+          }
         }
-      }
-    );
+      );
 
-    results.push({
-      type: "Article",
-      id: "Priberam" + results.length,
-      title: "Priberam",
-      thumb_url:
-        "https://img.ibxk.com.br/2014/2/programas/9695104192743330.png",
-      description:
-        definitionPriberam.word +
-        " " +
-        definitionPriberam.definition[0].definition,
-      input_message_content: {
-        parse_mode: "HTML",
-        message_text:
-          "<b><i>" +
+      results.push({
+        type: "Article",
+        id: "Priberam" + results.length,
+        title: "Priberam",
+        thumb_url:
+          "https://img.ibxk.com.br/2014/2/programas/9695104192743330.png",
+        description:
           definitionPriberam.word +
-          "</i></b> \n" +
-          definitionsPriberam.join("\n") +
-          '\n\n <a href="' +
-          definitionPriberam.source +
-          '">Source</a>',
-      },
-    });
+          " " +
+          definitionPriberam.definition[0].definition,
+        input_message_content: {
+          parse_mode: "HTML",
+          message_text:
+            "<b><i>" +
+            definitionPriberam.word +
+            "</i></b> \n" +
+            definitionsPriberam.join("\n") +
+            '\n\n <a href="' +
+            definitionPriberam.source +
+            '">Source</a>',
+        },
+      });
+    }
   }
   return results;
 };
