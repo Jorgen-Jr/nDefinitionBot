fec17f040884456de638465d0e6c6889d1008c27
Jorgen-Jr <jorgenjr@outlook.com>
2021-01-04 14:44:24 -0300

    Removed useless response var.

--- functions/bot.js
+++ functions/bot.js
@@ -74,18 +74,18 @@ exports.handler = async event => {
     const req = JSON.parse(body);
 
     const {
-        update_id,
+        // update_id,
         message,
-        edited_message,
-        channel_post,
-        edited_channel_post,
+        // edited_message,
+        // channel_post,
+        // edited_channel_post,
         inline_query,
-        chosen_inline_result,
-        callback_query,
-        shipping_query,
-        pre_checkout_query,
-        poll,
-        poll_answer,
+        // chosen_inline_result,
+        // callback_query,
+        // shipping_query,
+        // pre_checkout_query,
+        // poll,
+        // poll_answer,
     } = req;
 
     console.log('Update received: ', body, req);
